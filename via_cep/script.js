4ba9ee0deea50259db408be1ca4603e3b362a6d7
Lucas Herrero <lucasherrerosilva.lh@gmail.com>
2024-12-14 13:35:09 -0300

    'adicionando máscara no input'

--- via_cep/script.js
+++ via_cep/script.js
@@ -1,18 +1,23 @@
+// espera a página carregar inteira antes de começar
 document.addEventListener('DOMContentLoaded', () => {
 
     let button = document.querySelector('#search-btn')
 
-    const searchCep = () => {
-        let cep = document.querySelector('#cep').value
-        let url = `https://viacep.com.br/ws/${cep}/json/`
+    let cep = document.querySelector('#cep')
+    $('#cep').mask('00000-000') //adicionando máscara no input
 
+    const searchCep = () => {
+        let url = `https://viacep.com.br/ws/${cep.value}/json/`
+
+        //requisição na api
         fetch(url)
-            .then(response => response.json())
+            .then(response => response.json()) //transformando resposta de texto pra json
             .then(data => {
                 showAddress(data)
             })
     }
 
+    //função pra pegar os dados e jogar nos inputs
     const showAddress = (data) => {
         let address = document.querySelector('#address')
         let neighbor = document.querySelector('#neighbor')
